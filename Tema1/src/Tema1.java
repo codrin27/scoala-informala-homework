f1bd38a8dd525c396d2aa553a039efdda9e4be73
codrin27 <codrinbanu@yahoo.com>
2019-01-13 14:44:08 +0200

    Tema2. Exercitiile 1,2,3.

--- Tema1/src/Tema1.java
+++ Tema1/src/Tema1.java
@@ -2,28 +2,53 @@ import java.util.Scanner;
 public class Tema1 {
     public static void main(String[] args) {
         ex1();
-        ex2();
+        int ex2 = Ex2_3.maxim();
+        int ex3 = Ex2_3.minimum();
+        System.out.println(ex2);
+        System.out.println(ex3);
     }
-    private static void ex1(){
+
+    private static void ex1() {
         int n = (99 * 100) / 2;
         System.out.println("Suma primelor 100 de numere mai mari ca 0 este= " + n);
     }
-    private static void ex2(){
-        int max;
-        Scanner scrie = new Scanner(System.in);
-        System.out.println("Introduceti numarul de elemente al sirului.");
-        int n = scrie.nextInt();
-        int[] array;
-        array = new int[n];
-        for(int i = 0; i < n; i++){
-            array[i] = scrie.nextInt();
-        }
-        max = array[0];
-        for (int i = 1; i <= n; i++){
-            if(max < array[i]){
-                max = array[i];
+
+    private static class Ex2_3 {
+        private static int maxim() {
+            int max;
+            Scanner scrie = new Scanner(System.in);
+            System.out.println("Introduceti numarul de elemente al sirului.");
+            int n = scrie.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = scrie.nextInt();
             }
+            max = array[0];
+            for (int i = 1; i < array.length; i++)
+                if (max < array[i]) {
+                    max = array[i];
+                }
+            System.out.println("Cel mai mare numar din sirul dumneavoastra este= " + max);
+            return max;
         }
-        System.out.println("Cel mai mare numar din sirul dumneavoastra este= "+ max);
+
+        private static int minimum() {
+            int min;
+            Scanner scrie = new Scanner(System.in);
+            System.out.println("Introduceti numarul de elemente al sirului.");
+            int n = scrie.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = scrie.nextInt();
+            }
+            min = array[0];
+            for (int i = 1; i < array.length; i++)
+                if (min > array[i]) {
+                    min = array[i];
+                }
+            System.out.println("Cel mai mic numar din sirul dumneavoastra este= " + min);
+            return min;
+        }
     }
 }
+
